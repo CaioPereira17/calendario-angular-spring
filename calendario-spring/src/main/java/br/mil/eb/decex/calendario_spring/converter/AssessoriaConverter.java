4c5167a637867e0fc62810fef2b6e9249916054f
Caio <caiosilvaofrj@gmail.com>
2026-01-06 11:06:38 -0300

    Limpeza e organização.

--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/converter/AssessoriaConverter.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/converter/AssessoriaConverter.java
@@ -1,26 +1,3 @@
-// package br.mil.eb.decex.calendario_spring.converter;
-
-// import org.springframework.core.convert.converter.Converter;
-
-// import br.mil.eb.decex.calendario_spring.modelo.Assessoria;
-// import io.micrometer.common.util.StringUtils;
-
-// public class AssessoriaConverter implements Converter<String, Assessoria>{
-
-//     @Override
-// 	public Assessoria convert(@SuppressWarnings("null") String id) {
-// 		if (!StringUtils.isEmpty(id)) {
-// 			Assessoria asse = new Assessoria();
-// 			asse.setId(Long.valueOf(id));
-// 			return asse;
-// 		}
-		
-// 		return null;
-    
-// }
-
-// }
-
 package br.mil.eb.decex.calendario_spring.converter;
 
 import org.springframework.core.convert.converter.Converter;
